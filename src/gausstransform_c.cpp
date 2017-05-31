7f0b3faf2d4993fbb21d2267c5bbc98618bb9b72
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-05-31 15:25:42 +0200

    add timing

--- src/gausstransform_c.cpp
+++ src/gausstransform_c.cpp
@@ -1,16 +1,13 @@
 #include <math.h>
 
-//declaration
-//template<typename T>
-//T GaussTransform(const T* A, const T* B, int m, int n,
-//    int dim, T scale, T* grad);
-
 //definition
 #include "gausstransform_ref.cpp"
 
-//compile-time instantation
-//double GaussTransform(const double* A, const double* B, int m, int n, int dim, double 
-//        scale, double* grad);
+
+
+//use openmp just for measuring time
+#include <omp.h>
+
 
 //extern C interface
 extern "C" {
@@ -18,6 +15,15 @@ extern "C" {
 float call_GaussTransform(double* cost, const double* A, const double* B, int m, int n, int dim, double 
         scale, double* grad) {
     *cost = GaussTransform(A, B, m, n, dim, scale, grad);
+    return 0.0;
+}
+
+
+float time_GaussTransform(double* cost, const double* A, const double* B, int m, int n, int dim, double 
+        scale, double* grad) {
+    double start = omp_get_wtime();
+    *cost = GaussTransform(A, B, m, n, dim, scale, grad);
+    return (float)((omp_get_wtime() - start)*1e3);
 }
 
 
